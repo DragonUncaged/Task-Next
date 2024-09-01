6e1ac89788fe82a91d01409adc7b4c7bf3e9e8cc
Ankit <emailforreal.ankit@gmail.com>
2024-09-01 23:20:34 +0530

    Fixed-#3

--- src/pages/api/delete.ts
+++ src/pages/api/delete.ts
@@ -1,20 +1,17 @@
-import { prisma } from "../../lib/prisma";
+import { prisma } from "../../src/components/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const { id, name }=req.body;
+    const id=req.body;
 
     try {
-        await prisma.task.update({
-            data: {
-                name
-            },
+        await prisma.task.delete({
             where: {
                 id: id
             }
         });
 
-        res.status(200).json({ message: 'Updated' });
+        res.status(200).json({ message: 'Deleted' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: error });
--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -1,17 +1,20 @@
-import { prisma } from "../../lib/prisma";
+import { prisma } from "../../src/components/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const id=req.body;
+    const { id, name }=req.body;
 
     try {
-        await prisma.task.delete({
+        await prisma.task.update({
+            data: {
+                name
+            },
             where: {
                 id: id
             }
         });
 
-        res.status(200).json({ message: 'Deleted' });
+        res.status(200).json({ message: 'Updated' });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: error });
